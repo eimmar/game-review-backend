8aee7f928c8d4e1c31e2bc268d6cc2cc8bef7985
Eimantas Markevičius <eimantas.markeviius@nfq.lt>
2020-04-24 01:10:58 +0300

    Added username validation

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -13,13 +13,20 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username', EmailType::class, ['constraints' => [new NotBlank(), new Length(['min' => 4])]])
+            ->add('username', TextType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Regex(['pattern' => '/^[a-zA-Z0-9_]+$/']),
+                    new Length(['min' => 4])
+                ]
+            ])
             ->add('email', EmailType::class, ['constraints' => [new NotBlank(), new Email()]])
             ->add('firstName', TextType::class, ['constraints' => [new NotBlank()]])
             ->add('lastName')
